8da4a6b142e010fca54da8ceeb39100502c47c05
dinelly43 <valdirdinelly@hotmail.com>
2024-03-21 11:56:26 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
+let lista = [];
+
 function getCategories(){
     fetch('https://fakestoreapi.com/products/categories')
     .then(response => response.json())
     .then(response =>{
+        categorias.innerHTML = `<option>todos</option>`
         for(let i = 0; i < response.length; i++){
             categorias.innerHTML += `<option>${response[i]}</option>`;
         }
@@ -15,25 +18,42 @@ async function getProducts(){
     try {
         const request = await fetch('https://fakestoreapi.com/products');
         const response = await request.json();
-        produtos.innerHTML = '';
-        response.map(produto => {
-            produtos.innerHTML += 
-        `<li class="card">
-            <div class="card-imagem">
-                <img src="${produto.image}" alt="">
-                <div class="card-nota">${produto.rating.rate}</div>
-            </div>
-            <div class="card-info">
-                <h3>${produto.title}</h3>
-                <h6>${produto.category}</h6>
-                <h4>$${produto.price}</h4>
-
-            </div>
-        </li>`;
-        })
+        lista = (response)
+        renderlist(response);
         return;
     } catch (error) {
         alert("produtos não encontrados :/")
     }
 } 
 getProducts();
+
+function orderingBy(){
+    renderlist(lista.toSorted((a, b) => ordenacao.value == 1 ? a.price - b.price : b.rating.rate - a.rating.rate))
+}
+
+function filteredBy(){
+    if(categorias.value === 'todos'){
+        renderlist(lista)
+    } else{
+        renderlist(lista.filter((item) => item.category == categorias.value))
+    }
+}
+
+function renderlist(arr){
+    produtos.innerHTML = '';
+    arr.map(produto => {
+        produtos.innerHTML += 
+    `<li class="card">
+        <div class="card-imagem">
+            <img src="${produto.image}" alt="">
+            <div class="card-nota">${produto.rating.rate}</div>
+        </div>
+        <div class="card-info">
+            <h3>${produto.title}</h3>
+            <h6>${produto.category}</h6>
+            <h4>$${produto.price}</h4>
+
+        </div>
+    </li>`;
+    })
+}
